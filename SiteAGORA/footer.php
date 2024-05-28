fb9dd72a047252e83cc531818e230fb069c30284
romeo-rf <63726004+romeo-rf@users.noreply.github.com>
2024-05-28 11:54:01 +0200

    Delete SiteAGORA directory

--- SiteAGORA/footer.php
+++
@@ -1,3 +0,0 @@
-<a class="nav-link" href="faq.php">FAQ</a>
-<a class="nav-link" href="privacy_policy.php">Politique de Confidentialité</a>
-<a class="nav-link" href="terms.php">Conditions Générales d'Utilisation</a>
